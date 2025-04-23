6b8a8d02b89ed2dd96752083bd901ecbb83a0bd1
Phantom-VK <vikramadityakhupse@gmail.com>
2025-04-23 20:57:20 +0530

    Set up for Railway cloud deployment

--- FirstDjangoProject/FirstDjangoProject/settings.py
+++ FirstDjangoProject/FirstDjangoProject/settings.py
@@ -14,8 +14,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-CSRF_TRUSTED_ORIGINS= ["https://djangoprojects-production.up.railway.app/"]
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://djangoprojects-production.up.railway.app'
+]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -28,8 +29,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -40,10 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Home',
-    'Register'
+    'Register',
+    'corsheaders'
+
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,10 +53,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'FirstDjangoProject.urls'
+CORS_ALLOWED_ORIGINS = [
+    'https://djangoprojects-production.up.railway.app',
+]
 
 TEMPLATES = [
     {
@@ -74,7 +79,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'FirstDjangoProject.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -84,7 +88,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -104,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -115,7 +117,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
