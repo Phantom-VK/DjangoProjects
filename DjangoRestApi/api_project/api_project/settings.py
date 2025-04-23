3f9d29e0a48b705c76b9ad0783f010601afbe5c3
Phantom-VK <vikramadityakhupse@gmail.com>
2025-04-23 20:16:58 +0530

    Set up for Railway cloud deployment

--- DjangoRestApi/api_project/api_project/settings.py
+++ DjangoRestApi/api_project/api_project/settings.py
@@ -127,7 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  # ✅ correct
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
