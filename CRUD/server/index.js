2bf6ad6f56de661db30de05e35168f541c3b5aac
Phantom-VK <vikramadityakhupse@gmail.com>
2025-04-24 11:36:23 +0530

    Changes to deployment

--- CRUD/server/index.js
+++ CRUD/server/index.js
@@ -1,27 +1,34 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path'); 
 const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
+// API Routes
 const userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes);
 
-// MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/timecapsule')
+mongoose.connect('mongodb+srv://2022bit052:OOad5MumuWkJfh02@cluster0.noxkp4q.mongodb.net/your-database-name?retryWrites=true&w=majority')
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('MongoDB connection error:', err));
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 // Basic route
 app.get('/', (req, res) => {
-    res.send('MERN CRUD Backend Running!');
+  res.send('MERN CRUD Backend Running!');
 });
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
