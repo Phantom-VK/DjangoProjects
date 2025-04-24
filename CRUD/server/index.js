d56bc13940e7edc50aef2b015c288f1a379fc2c1
Phantom-VK <vikramadityakhupse@gmail.com>
2025-04-24 10:36:56 +0530

    Made backend ready for deployment

--- CRUD/server/index.js
+++ CRUD/server/index.js
@@ -11,7 +11,7 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/users', userRoutes);
 
 // MongoDB Connection
-mongoose.connect('mongodb://127.0.0.1:27017/mern_crud')
+mongoose.connect('mongodb://localhost:27017/timecapsule')
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('MongoDB connection error:', err));
 
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
